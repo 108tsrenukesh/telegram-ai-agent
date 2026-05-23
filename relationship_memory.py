4a2ad9c9282ed2a5c89adfa3ef47e9edfab3c474
T S RENUKESH <tsrenukesh@gmail.com>
2026-05-23 20:54:59 +0530

    fix: add logging to all except blocks

--- relationship_memory.py
+++ relationship_memory.py
@@ -1,74 +1,64 @@
 import json
+import logging
 
+logger = logging.getLogger(__name__)
 
-MEMORY_FILE = (
-    "relationship_memory.json"
-)
+MEMORY_FILE = "relationship_memory.json"
 
 
 def load_relationship_memory():
 
     try:
 
-        with open(
-            MEMORY_FILE,
-            "r"
-        ) as file:
+        with open(MEMORY_FILE, "r") as file:
 
             return json.load(file)
 
+    except FileNotFoundError:
+
+        return {}
+
     except Exception:
+
+        logger.exception(
+            "load_relationship_memory failed [file=%s]",
+            MEMORY_FILE
+        )
 
         return {}
 
 
 def save_relationship_memory(data):
 
-    with open(
-        MEMORY_FILE,
-        "w"
-    ) as file:
+    try:
 
-        json.dump(
-            data,
-            file,
-            indent=4
+        with open(MEMORY_FILE, "w") as file:
+
+            json.dump(data, file, indent=4)
+
+    except Exception:
+
+        logger.exception(
+            "save_relationship_memory failed [file=%s]",
+            MEMORY_FILE
         )
 
 
-def update_relationship_memory(
+def update_relationship_memory(person, topic):
 
-    person,
-    topic
-
-):
-
-    memory = (
-        load_relationship_memory()
-    )
+    memory = load_relationship_memory()
 
     if person not in memory:
 
         memory[person] = {
-
             "topics": [],
             "interaction_count": 0
-
         }
 
-    if (
-        topic
-        not in memory[person]["topics"]
-    ):
+    if topic not in memory[person]["topics"]:
 
-        memory[person]["topics"].append(
-            topic
-        )
+        memory[person]["topics"].append(topic)
 
-    memory[person][
-        "interaction_count"
-    ] += 1
+    memory[person]["interaction_count"] += 1
 
-    save_relationship_memory(
-        memory
-    )
+    save_relationship_memory(memory)
